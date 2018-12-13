11a8d2d684200239eb8601c0cfa05e48511a7f4f
Theo Danneels <theo.danneels@telecom-paristech.fr>
2018-12-13 23:31:05 +0100

    Rewrite the fzf functions

--- fish/config.fish
+++ fish/config.fish
@@ -32,7 +32,8 @@ end
 
 
 # FZF configuration
-set -gx FZF_ALT_C_COMMAND "echo 4"
+set -gx FZF_DEFAULT_COMMAND "fd . $HOME -E $HOME/Library"
+set -gx FZF_ALT_C_COMMAND "fd -t d -E Library -E Applications"
 set -gx FZF_CTRL_T_COMMAND "fd -t f -E Library -E Applications"
 
 # Default editor is nvim
