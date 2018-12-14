4430bb696d0cac93edfc5afe611f3ee71a669eb8
Theo Danneels <theo.danneels@telecom-paristech.fr>
2018-12-15 00:01:56 +0100

    Rearrange fzf related env in fish files

--- fzf/fzf.fish
+++ fzf/fzf.fish
@@ -1,3 +1,9 @@
+# FZF configuration
+set -gx FZF_DEFAULT_COMMAND "fd . $HOME -E $HOME/Library"
+set -gx FZF_ALT_C_COMMAND "fd -t d -E Library -E Applications"
+set -gx FZF_CTRL_T_COMMAND "fd -t f -E Library -E Applications"
+
+# FZF colors
 set -x FZF_DEFAULT_OPTS "
   --color=bg+:$background,bg:$background,spinner:$purple,hl:$purple
   --color=fg:$foreground,header:$foreground,info:$yellow,pointer:$purple
