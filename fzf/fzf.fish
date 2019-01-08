546aa766fa579f6371dd9bf9311f11ab5901a18a
Theo Danneels <theo.danneels@telecom-paristech.fr>
2019-01-08 18:42:28 +0100

    Fix fzf colors

--- fzf/fzf.fish
+++ fzf/fzf.fish
@@ -1,8 +1,9 @@
 set -l foreground '#ebdbb2'
 set -l background '#282828'
 set -l comment '#928374'
-set -l blue '#458588'
-set -l red '#cc241d'
+set -l blue '#83a598'
+set -l red '#fb4934'
+set -l aqua '#8ec07c'
 
 # FZF configuration
 set -gx FZF_DEFAULT_COMMAND "fd . $HOME -E $HOME/Library"
@@ -12,6 +13,6 @@ set -gx FZF_CTRL_T_COMMAND "fd -t f -E Library -E Applications"
 # FZF colors
 set -x FZF_DEFAULT_OPTS "
   --color=bg+:$background,bg:$background,spinner:$red,hl:$blue
-  --color=fg:$foreground,header:$foreground,info:$comment,pointer:$red
-  --color=marker:$red,fg+:'#ffffff',prompt:$comment,hl+:$blue
+  --color=fg:$foreground,header:$foreground,info:$aqua,pointer:$red
+  --color=marker:$red,fg+:$foreground,prompt:$aqua,hl+:$blue
 "
