77891c606105aaa3f0e8f80eaab94e263060416a
Theo Danneels <danneels.theo@gmail.com>
2019-11-26 00:20:30 +0100

    Update VSCode

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,9 +1,11 @@
 code --install-extension bmalehorn.vscode-fish
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension dramforever.vscode-ghc-simple
 code --install-extension EditorConfig.EditorConfig
+code --install-extension hoovercj.haskell-linter
 code --install-extension ilyakooo0.ormolu
 code --install-extension JakeBecker.elixir-ls
-code --install-extension jcanero.hoogle-vscode
+code --install-extension James-Yu.latex-workshop
 code --install-extension jdinhlife.gruvbox
 code --install-extension justusadam.language-haskell
 code --install-extension KevinRose.vsc-python-indent
@@ -18,7 +20,11 @@ code --install-extension PKief.material-icon-theme
 code --install-extension Prisma.prisma
 code --install-extension redhat.java
 code --install-extension sbrink.elm
-code --install-extension Vans.haskero
-code --install-extension vigoo.stylish-haskell
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
+code --install-extension vscjava.vscode-maven
 code --install-extension vscodevim.vim
 code --install-extension waderyan.gitblame
