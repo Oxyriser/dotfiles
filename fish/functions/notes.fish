b76cbc547d4f89b018eacb3a1a01f41331c87e69
Theo Danneels <danneels.theo@gmail.com>
2020-05-13 00:05:27 +0200

    Update fish notes function

--- fish/functions/notes.fish
+++ fish/functions/notes.fish
@@ -2,9 +2,16 @@ function notes
   set -l name "notes"
   count $argv > /dev/null; and set name $argv[1]
 
-  wkhtmltopdf (pandoc --from=markdown+raw_html+markdown_in_html_blocks \
-                 --template=github.html5 \
-                 $name.md \
-              | psub -s .html) \
-              $name.pdf
+  pandoc $name.md -o $name.pdf \
+  --pdf-engine=xelatex \
+  --table-of-contents \
+  --from=markdown \
+  --number-sections \
+  --indented-code-classes=javascript \
+  --highlight-style=monochrome \
+  -V mainfont="Palatino" \
+  -V documentclass=report \
+  -V papersize=A5 \
+  -V geometry:margin=1in
+
 end
